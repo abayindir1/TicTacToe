283f6d3b384c1c018cdc9b87e5e7d843a07bcc41
Abdullah Bayindir <byndrabdullah@gmail.com>
2022-05-20 15:32:12 -0500

    made game board mutable

--- ttt.py
+++ ttt.py
@@ -2,5 +2,14 @@ game = [[0,0,0],
         [0,0,0],
         [0,0,0]]
 
-for row in game:
-    print(row)
+def gameBoard(board, player, row, column):
+    if player != 0:
+        print("   0  1  2")
+        board[row][column] = player   
+        for count, row in enumerate(board):
+            print(count, row)
+        return board 
+    else:
+        print("You cannot play")   
+
+gameBoard(game, 1, 0 ,1)
