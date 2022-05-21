4d22b9bb9b79d1fb4c3d4660931d3c4a313bb2dd
Abdullah Bayindir <byndrabdullah@gmail.com>
2022-05-21 13:41:50 -0500

    check horizontal win

--- ttt.py
+++ ttt.py
@@ -1,15 +1,32 @@
-game = [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
 
-def gameBoard(board, player, row, column):
-    if player != 0:
-        print("   0  1  2")
-        board[row][column] = player   
-        for count, row in enumerate(board):
-            print(count, row)
-        return board 
-    else:
-        print("You cannot play")   
+game1 = [[1, 0, 1],
+        [2, 2, 2],
+        [0, 2, 0]]
 
-gameBoard(game, 1, 0 ,1)
+def horizontalWin(currentGame):
+        for row in currentGame:
+                result = all(element == row[0] for element in row)
+                if result:
+                        print(row)
+                        print("You won!")
+                else: 
+                        print(row)
+
+
+# def gameBoard(board, player, row, column, display=False):
+#     try:
+#         if player != 0 and not display:
+#             print("   0  1  2")
+#             board[row][column] = player
+#             for count, row in enumerate(board):
+#                 print(count, row)
+#             return board
+#         else:
+#             print("You cannot play")
+#     except IndexError as e:
+#         print("Error: make sure you input row/column as 0 1 or 2", e)
+
+horizontalWin(game1)
